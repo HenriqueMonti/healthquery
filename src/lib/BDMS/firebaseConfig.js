eaf28f472c144f9d9b1a7f3c054baaab65067fb8
XxXRodrigoXx <rodrigopenagg@gmail.com>
2024-06-18 22:09:27 -0400

    Create firebaseConfig.js
    Conexão com Banco Dados

---
+++ src/lib/BDMS/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAuqFJhTiKYRHOUBdRK_IMkqjnPoGLXQR8",
+    authDomain: "healthquery-c5256.firebaseapp.com",
+    databaseURL: "https://healthquery-c5256-default-rtdb.firebaseio.com",
+    projectId: "healthquery-c5256",
+    storageBucket: "healthquery-c5256.appspot.com",
+    messagingSenderId: "825208819328",
+    appId: "1:825208819328:web:ad264ee30e6ac61d5e2065",
+    measurementId: "G-5PHMWC8C4F"
+  };
